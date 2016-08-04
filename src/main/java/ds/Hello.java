877d5c8ed58dcc37d377bb270b87910d92af476b
shahprincea <shahprincea@gmail.com>
2016-08-04 15:57:44 -0700

    Initial commit

---
+++ src/main/java/ds/Hello.java
@@ -0,0 +1,11 @@
+package main.java.ds;
+
+/**
+ * Created by prince on 8/4/16.
+ */
+public class Hello {
+
+    public static void main(String... args) {
+        System.out.println("Hello World");
+    }
+}
